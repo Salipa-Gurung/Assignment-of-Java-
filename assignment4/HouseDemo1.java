34c26ee8fc0cab7d84fbc1fd87373295ffbe65f2
Salipa-Gurung <salipagurung@gmail.com>
2020-03-15 07:07:30 -0700

    Sun Added.

--- assignment4/HouseDemo1.java
+++ assignment4/HouseDemo1.java
@@ -1,25 +1,20 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.applet.Applet;
-import java.awt.Color;
-import java.awt.Graphics;
-import javax.swing.*;
 
-public class HouseDemo1 extends JFrame
+public class HouseDemo1 extends Canvas
 {
 
-	HouseDemo1()
+	/*HouseDemo1()
 	{
-		// super("2D House Demo");
+		super("2D House Demo");
 		setSize(1500,900);
 		setVisible(true);
 		setBackground(new Color(0,255,0));
-	}
+	}   */
 
 	public void paint(Graphics g)
 	{
-		// super.paint(g);
-		//g.fillRoundRect(15,40,80,55,50,50);
+		//super.paint(g);
 		g.setColor(Color.red);
 		g.fill3DRect(550,350,300,200,false);
 		g.setColor(Color.black);
@@ -28,38 +23,34 @@ public class HouseDemo1 extends JFrame
 		g.setColor(Color.white);
 		g.fill3DRect(625,410,150,70,false);
 
-	/*	g.setColor(Color.black);
-		g.fillArc(400,100,400,200,60,-170);  */
+		g.setColor(new Color(160, 110, 40));
+		int[] x= {500,700,900,500};
+		int[] y= {350,210,350,350};
+	
+		g.fillPolygon(x,y,4);   
 
 		g.setColor(Color.black);
 		g.drawRect(625,410,150,70);
 		g.drawLine(662,410,662,480);
 		g.drawLine(699,410,699,480);
 		g.drawLine(736,410,736,480);
+		g.drawPolygon(x,y,4);
 
-
+		g.setColor(Color.yellow);
+		g.fillOval(1000,90,70,70);
 		// g.drawLine(500,350,700,210);
 		// g.drawLine(700,210,900,350);
-		// g.drawLine(500,350,900,350);
-		g.setColor(new Color(155, 105, 35));
-		int[] x= {500,700,900,500};
-		int[] y= {350,210,350,350};
-	
-	//	g.drawPolygon(x,y,3);
-		// g.drawPolygon(500, 350, 700, 210, 900, 350, 500, 350);
-		g.fillPolygon(x,y,4);      
-
-		//g.drawOval(195,40,80,55);
+		// g.drawLine(500,350,900,350);     
 	}
 
 	public static void main(String[] args) 
 	{
 		HouseDemo1 demo=new HouseDemo1();
-	
-		demo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			Frame f=new Frame();
+
+			f.setSize(1500,900);
+			f.setVisible(true);
+			f.setBackground(new Color(0,100,0));
+			f.add(demo);
 	}
 }
-/* 
-<applet code="SetBackColor" width=200 height=200>
-</applet>
-*/
